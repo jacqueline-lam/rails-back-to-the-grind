6c323586850fc9fa12e1f0ef0021a0bd52c4f679
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-03-06 13:59:49 -0500

    First pass at cafe scraper. Will adopt if I decide to scrape online data

---
+++ app/adapter/cafe_scraper.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class CafeScraper
+  # def scrape_cafes
+    # doc = Nokogiri::HTML(open(#URL))
+    # attrs_arr = []
+    # doc.css().each do ||
+    # end
+    # writing data to a JSON file in JSON format in case article changes/ deletes data; 
+    # no need to scrape it everytime
+    # save to file and have it locally and script will be run once
+    # File.write('cafes.json', attrs_arr.to_json)
+  # end
+end
